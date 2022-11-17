ea8c8322c5221e98e4afe1dc8ab03062d94f919d
unknown <sanders.yorkshire.150b@gmail.com>
2022-11-17 10:09:57 +0530

    Initial Commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Footer: FC = () => {
+    return(
+        <h1>
+            Footer Goes here!
+        </h1>
+    );
+};
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Header: FC = () => {
+    return(
+        <h1>
+            Header Goes Here !
+        </h1>
+    );
+};
+
+export default Header;
